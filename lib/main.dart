c8a11f7ee1f4f17d12ea18fe6ae8f33a45492a3f
skai <skaidevs@gmail.com>
2019-08-29 04:16:28 -0500

    Project sign in fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:food_delivery/bloc/cartListBloc.dart';
+import 'package:food_delivery/model/foodItem.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,10 +29,165 @@ class Home extends StatelessWidget {
           child: ListView(
             children: <Widget>[
               FirstHalf(),
+              SizedBox(
+                height: 45,
+              ),
+              for (var foodItem in foodItemList.foodItems)
+                ItemContainer(foodItem: foodItem)
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class ItemContainer extends StatelessWidget {
+  final FoodItem foodItem;
+
+  ItemContainer({@required this.foodItem});
+
+  final CartListBloc bloc = BlocProvider.getBloc<CartListBloc>();
+
+  addToCart(FoodItem foodItem) {
+    bloc.addToList(foodItem);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        addToCart(foodItem);
+
+        final snackBar = SnackBar(
+          content: Text('${foodItem.title} added to the cart'),
+          duration: Duration(milliseconds: 1000),
+        );
+
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      child: Items(
+          hotel: foodItem.hotel,
+          itemName: foodItem.title,
+          itemPrice: foodItem.price,
+          imgUrl: foodItem.imgUrl,
+          leftAligned: foodItem.id % 2 == 0 ? true : false),
+    );
+  }
+}
+
+class Items extends StatelessWidget {
+  Items(
+      {@required this.leftAligned,
+      @required this.imgUrl,
+      @required this.itemName,
+      @required this.itemPrice,
+      @required this.hotel});
+
+  final bool leftAligned;
+  final String imgUrl;
+  final String itemName;
+  final double itemPrice;
+  final String hotel;
+
+  @override
+  Widget build(BuildContext context) {
+    double containerPadding = 45;
+    double containerBorderRadius = 10;
+
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(
+            left: leftAligned ? 0 : containerPadding,
+            right: leftAligned ? containerPadding : 0,
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.horizontal(
+                    left: leftAligned
+                        ? Radius.circular(0)
+                        : Radius.circular(containerBorderRadius),
+                    right: leftAligned
+                        ? Radius.circular(containerBorderRadius)
+                        : Radius.circular(0),
+                  ),
+                  child: Image.network(
+                    imgUrl,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                  left: leftAligned ? 20 : 0,
+                  right: leftAligned ? 0 : 20,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            itemName,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "\$$itemPrice",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: RichText(
+                        text: TextSpan(
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontSize: 15,
+                            ),
+                            children: [
+                              TextSpan(text: 'by '),
+                              TextSpan(
+                                text: hotel,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ]),
+                      ),
+                    ),
+                    SizedBox(
+                      height: containerPadding,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
